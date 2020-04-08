3d8e93f24acf929b6159002a0c1b3e96927e152a
sara ebadi <ebadi.sara227@gmail.com>
2020-04-08 18:01:11 +0430

    Refactor code

--- app/src/main/java/com/saraebadi/github/roozeto/features/morningalarm/MorningAlarmReceiver.java
+++ app/src/main/java/com/saraebadi/github/roozeto/features/morningalarm/MorningAlarmReceiver.java
@@ -8,17 +8,13 @@ import android.os.Build;
 import com.saraebadi.github.roozeto.features.morningalarm.MorningService;
 
 public class MorningAlarmReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
-
         Intent intentReciver = new Intent(context, MorningService.class);
-
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             context.startForegroundService(intentReciver);
         }else {
             context.startService(intentReciver);
         }
     }
-
 }
